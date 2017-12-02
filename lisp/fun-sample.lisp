4da26805acbd904dadca57d9bdc5ea03295a52a6
Zhao Jinlong <kinryu.cho@nsij.org>
2017-12-02 16:38:51 +0900

    update fun-sample

--- lisp/fun-sample.lisp
+++ lisp/fun-sample.lisp
@@ -18,3 +18,55 @@
 (defun alias-param (&key ((:apple a)) ((:banana b) 10) ((:chicken c)))
   "In this function I give the parameter a alias. Then you will use (alias-param :apple 1 :banana 2 :chicken 3) to call this function."
   (list a b c))
+
+(defun mixed-param (x &optional y &key z)
+  "Don't use this code in your code, beacause mix optional to key parameter will lead to somewhat surprising result."
+  (list x y z))
+
+(defun mixed-param-optional-rest (x &optional y &rest z)
+  "You can see if you don't supply the optional parameter, they will eat up the value for rest parameter. 
+If you don't want that you must supply the NIL to optional paramter if you don't want to use them, so they will not be optional."
+  (list x y z))
+
+(defun mixed-param-optional (x &optional y z)
+  "This is a right sample to mix parameters."
+  (list x y z))
+
+(defun mixed-param-rest (x y &rest z)
+  "This also a right sample to mix parameters."
+  (list x y z))
+
+(defun mixed-param-rest-key (&rest rest &key a b c)
+  "Your can see that it's not a good idea to mix rest and key"
+  (list rest a b c))
+
+(defun foo (n)
+  "Notice that this is a wrong code."
+  (dotimes (i 10)
+    (dotimes (j 10)
+      (when (> (* i j) n)
+	(list i j)))))
+
+(defun foo (n)
+  "And this is right, if i * j equals n, return the i and j "
+  (dotimes (i 10)
+    (dotimes (j 10)
+      (when (> (* i j) n)
+	(return-from foo (list i j))))))
+
+(defun plot (fn min max step)
+  (loop for i from min to max by step do
+       (loop repeat (funcall fn i) do (format t "*"))
+       (format t "~%")))
+
+(defun apply-plot(plot-data)
+  "You can call plot with a list variable using apply.
+  call this use (apply-plot (list #'exp 0 4 1/2))"
+  (apply #'plot plot-data))
+
+(defun apply-plot-ex (&rest plot-data)
+  "If you want to call apply-plot like calling plot use this."
+  (apply #'plot plot-data))
+
+(defun plot-lambda ()
+  (plot #'(lambda (x) (* x 2)) 0 10 1))
