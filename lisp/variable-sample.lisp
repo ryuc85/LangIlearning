d1c3af8fba63ce16c4c628a8aa72a6dd97c1dbc1
Ryuc85 <marshorizon2015@gmail.com>
2017-12-03 20:52:40 +0900

    Add rust

--- lisp/variable-sample.lisp
+++ lisp/variable-sample.lisp
@@ -19,3 +19,20 @@
 (defparameter *fn*
   (let ((count 0))
     #'(lambda () (setf count (1+ count)))))
+
+(defun increment-widget-count ()
+  (incf *count*))
+
+(defvar *x* 10)
+(defun foo ()
+  (format t "X: ~d~%" *x*))
+
+(defun foo-again ()
+  (format t "Before assignment~18tX: ~d~%" *x*)
+  (setf *x* (+ 1 *x*))
+  (format t "After assignment~18tX: ~d~%" *x*))
+
+(defun bar ()
+  (foo-again)
+  (let ((*x* 20)) (foo-again))
+  (foo-again))
---
+++ rust_practice/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Declare a new struct
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
