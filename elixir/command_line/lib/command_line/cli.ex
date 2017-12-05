884345a82fc418170ddb07b2237d951a0185c065
ryuc85 <kinr.chou@nsij.org>
2017-12-05 10:13:03 +0900

    Update elixir command line.

--- elixir/command_line/lib/command_line/cli.ex
+++ elixir/command_line/lib/command_line/cli.ex
@@ -1,5 +1,10 @@
 defmodule CommandLine.CLI do
-  @moduledoc false
+  @moduledoc """
+  The main module for the escript.
+  You need to set this module to project function in Mixfile.
+  ### Example >>>
+    escript: [main_module: CommandLine.CLI]
+"""
 
   @doc """
   The main function for the executable file.
@@ -7,5 +12,17 @@ defmodule CommandLine.CLI do
   """
   def main(args \\ []) do
     IO.puts("Hello world!")
+    # Let print and see which the args is like.
+    # There is a list of args. Like this ["hello", "world"]
+    IO.inspect(args)
+  end
+
+  @doc """
+  This function will parse the args,
+  use OptionParser.parse/2
+  """
+  def parse_args(args) do
+    # In next we will see how to parse the args
+
   end
 end
