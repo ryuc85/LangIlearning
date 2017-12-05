47aa3f2e60f5c34bed6653e09a8cb68d1073f304
ryuc85 <kinr.chou@nsij.org>
2017-12-06 08:49:33 +0900

    Create the sample of OptionParser

--- elixir/command_line/lib/command_line/cli.ex
+++ elixir/command_line/lib/command_line/cli.ex
@@ -1,10 +1,10 @@
 defmodule CommandLine.CLI do
   @moduledoc """
-  The main module for the escript.
-  You need to set this module to project function in Mixfile.
-  ### Example >>>
-    escript: [main_module: CommandLine.CLI]
-"""
+    The main module for the escript.
+    You need to set this module to project function in Mixfile.
+    ### Example >>>
+      escript: [main_module: CommandLine.CLI]
+  """
 
   @doc """
   The main function for the executable file.
@@ -14,7 +14,11 @@ defmodule CommandLine.CLI do
     IO.puts("Hello world!")
     # Let print and see which the args is like.
     # There is a list of args. Like this ["hello", "world"]
-    IO.inspect(args)
+    args
+    |> parse_args
+
+    # Next we will make a application that get the argumant output or input and the file path.
+    # If output is true we will output to a file which to the filepath, else we will read it.
   end
 
   @doc """
@@ -22,7 +26,43 @@ defmodule CommandLine.CLI do
   use OptionParser.parse/2
   """
   def parse_args(args) do
-    # In next we will see how to parse the args
+    # Here is parse only one args with OptionParser
+    # With this command: ./run --debug, we will get this: {[debug: true], [], []}
+    # ./run --debug false, we will get this: {[debug: "false"], [], []}
+    # If we set more args like this ./run --debug false hello world
+    # We will get this {[debug: "false"], ["hello", "world"], []}
+    #    args
+    #    |> OptionParser.parse(
+    #         switches: [
+    #           debug: :boolean
+    #         ]
+    #       )
 
+    # Now let we see how to parse multiply args.
+    # We use this command: ./run --debug --prefix /usr/local/xxx
+    # and we get this result: {[debug: true, prefix: "/usr/local/xxx"], [], []}
+    # So how can we get the :debug for the value false?
+    # Just pass it --no-debug
+    # When we pass the args like this ./run --no-debug --prefix /usr/local/xxx
+    # We will get {[debug: false, prefix: "/usr/local/xxx"], [], []}
+#    args
+#    |> OptionParser.parse(
+#         switches: [
+#           debug: :boolean,
+#           prefix: :string
+#         ]
+#       )
+#    |> IO.inspect
+
+    {opt, args, _} = args |>
+    OptionParser.parse(switches: [output: :boolean, input: :boolean, prefix: :string],
+      aliases: [o: :output, i: :input])
+    opt |> IO.inspect
+
+    cond do
+      opt[:output] == true -> IO.puts("That will output.")
+      opt[:input] == true -> IO.puts("That will input.")
+      true -> IO.puts("Default is output.")
+    end
   end
 end
